a9fd13bae25e84386a3ac2ee77d255cbf705a9f5
Colin Braun <electrolitic21@gmail.com>
2023-01-26 17:09:08 -0500

    Another change for the grader

--- controller.py
+++ controller.py
@@ -330,7 +330,7 @@ class Controller:
                 row[2] = -1
                 row[3] = 9999
             # If the switch is dead, consider its next hop to be -1
-            if not self.switch_statuses[row[0]] and row[1] == row[0]:
+            if not self.switch_statuses[row[0]]:
                 row[2] = -1
 
         self.rt_table = rt_table
